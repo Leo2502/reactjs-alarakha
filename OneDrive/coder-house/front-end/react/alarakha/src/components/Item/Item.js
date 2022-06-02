4a45dc6ec12b8e2225318e6590c8c5d068aa7e49
Leo2502 <leonel.sollazzo@gmail.com>
2022-06-02 00:45:10 -0300

    Edición en nombre de producto a mostrar

--- OneDrive/coder-house/front-end/react/alarakha/src/components/Item/Item.js
+++ OneDrive/coder-house/front-end/react/alarakha/src/components/Item/Item.js
@@ -5,7 +5,7 @@ const Item = ({item}) => {
 
     return (
         <div>
-            <h2 className="mb-3 nombreProducto">{item.nombre}</h2>
+            <h2 className="mb-3 nombreProducto">{item.nombre}_{item.id}</h2>
             <img src={item.img} alt={item.nombre} width="25%"/>
             <h4 className="mt-3">Precio: ${item.precio}</h4>
             <ItemCount item={item}/>

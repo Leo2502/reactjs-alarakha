dbdc8cbc801df7e6d75f9bab51d3041147222783
Leo2502 <leonel.sollazzo@gmail.com>
2022-06-10 00:54:32 -0300

    Cambios en estilos

--- OneDrive/coder-house/front-end/react/alarakha/src/components/NavbarComp/navbarComp.js
+++ OneDrive/coder-house/front-end/react/alarakha/src/components/NavbarComp/navbarComp.js
@@ -9,19 +9,19 @@ const NavbarComp = () => {
     return (
     <header className="header">
         <Navbar bg="light" expand="lg">
-            <Container className='header_container d-flex justify-content-around'>
+            <Container className='header_container'>
                 <Navbar.Brand className='w-25 p-3'><Link to={"/"}><img src='../assets/multimedia/images/general/logo.png' alt="logo" width="100%"/></Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <NavDropdown className='enlace' title="Catálogo" id="basic-nav-dropdown">
-                        <NavDropdown.Item className='enlace'><Link to={'categoria/brocheros'}>Brocheros</Link></NavDropdown.Item>
-                        <NavDropdown.Item className='enlace'><Link to={'categoria/carteras'}>Carteras</Link></NavDropdown.Item>
-                        <NavDropdown.Item className='enlace'><Link to={'categoria/mochilas'}>Mochilas</Link></NavDropdown.Item>
-                        <NavDropdown.Item className='enlace'><Link to={'categoria/neceseres'}>Neceseres</Link></NavDropdown.Item>
-                        <NavDropdown.Item className='enlace'><Link to={'categoria/portatodo'}>Portatodo</Link></NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item className='enlace'><Link to={'/'}>Ver todo</Link></NavDropdown.Item>
+                        <NavDropdown.Item><Link to={'categoria/brocheros'} className='enlace'>Brocheros</Link></NavDropdown.Item>
+                        <NavDropdown.Item><Link to={'categoria/carteras'} className='enlace'>Carteras</Link></NavDropdown.Item>
+                        <NavDropdown.Item><Link to={'categoria/mochilas'} className='enlace'>Mochilas</Link></NavDropdown.Item>
+                        <NavDropdown.Item><Link to={'categoria/neceseres'} className='enlace'>Neceseres</Link></NavDropdown.Item>
+                        <NavDropdown.Item><Link to={'categoria/portatodo'} className='enlace'>Portatodo</Link></NavDropdown.Item>
+                        <NavDropdown.Divider/>
+                        <NavDropdown.Item><Link to={'/'} className='enlace'>Ver todo</Link></NavDropdown.Item>
                     </NavDropdown>
                     <Link to={"/contacto"} className="header__navlink enlace">Contacto</Link>
                     <Link to={"/cart"} className='enlace'><CartWidget/></Link>

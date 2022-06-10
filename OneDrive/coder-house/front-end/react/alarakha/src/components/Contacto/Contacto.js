157bab9fbea130d30a9d3c1c9dc8c3642fda43d8
Leo2502 <leonel.sollazzo@gmail.com>
2022-06-09 21:43:33 -0300

    Detalles contacto

--- OneDrive/coder-house/front-end/react/alarakha/src/components/Contacto/Contacto.js
+++ OneDrive/coder-house/front-end/react/alarakha/src/components/Contacto/Contacto.js
@@ -1,11 +1,15 @@
+import './Contacto.scss'
+import { AiOutlineWhatsApp } from 'react-icons/ai'
+import { AiOutlineMail } from 'react-icons/ai'
+
 const Contacto = () => {
 
     return (
         <div className="contactos__container__item">
         <h3>Contactanos!</h3>
             <ul>
-                <li className="mb-3"><a href="https://wa.me/+5491162488937?text=Hola,%20quería%20consultar%20por%20" className="whatsapp" target="blank"><i className="fab fa-whatsapp"></i></a> +54-911-624-889-37</li>
-                <li><a href="mailto:alarakha.neceseres@hotmail.com"><i className="fas fa-at"></i></a> alarakha.neceseres@hotmail.com</li>
+                <li className="mb-3"><a href="https://wa.me/+5491162488937?text=Hola,%20quería%20consultar%20por%20" className="whatsapp" target="blank"><AiOutlineWhatsApp/></a> +54-911-624-889-37</li>
+                <li><a href="mailto:alarakha.neceseres@hotmail.com"><AiOutlineMail/></a> alarakha.neceseres@hotmail.com</li>
             </ul>
         </div>
     )
